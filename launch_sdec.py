5f5d18deebf5af7701d832b83179cade348e0982
Vinicius Paraizo <95001119+paraizovinicius@users.noreply.github.com>
2025-02-05 15:41:16 +0100

    Update launch_sdec.py

--- launch_sdec.py
+++ launch_sdec.py
@@ -1,10 +1,10 @@
-import torch
 import os
 from keras_sdec import DeepEmbeddingClustering
 import yaml
 import numpy as np
 import pandas as pd
-from processing_utils import transformDataInCategory
+from processing_utils import transformDataInCategory, divideDataset
+
 
 # Open the configuration file and load the different arguments
 with open('config.yaml') as f:
@@ -17,18 +17,23 @@ directory = config['output_embeddings']
 files = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.npy')]
 
 all_embeddings = []
-id_contratos = df['IdContrato'].astype(str).tolist()  # Ensure it's a list of strings
-
 for file_path in files: # for each batch saved ..
-    # Load the stored tuples of (index, embedding, contract_id)
+    # Load the stored tuples of (embedding, contract_id)
     embeddings = np.load(file_path)
     
     all_embeddings.extend(embeddings)
     
+
+    
 # Convert the list of embeddings into a large NumPy array
 X = np.vstack(all_embeddings)
-    
-unidades = df['Unidades']
+
+
+
+"""
+Adding unidades and elemdespesatce
+"""
+unidades = df['Unidade']
 elemdespesatce = df['ElemDespesaTCE']
 
 # unidades_one_hot is going to return a table with 1484918 rows and 771 columns
@@ -42,21 +47,49 @@ elemdespesatce_one_hot = transformDataInCategory(elemdespesatce)
 
 # Next steps: concatenate unidades_one_hot and elemdespesa_one_hot to 'all_embeddings'
 
-y = df['IdContrato'].astype(str).tolist()  # Ensure it's a list of strings
 
-# empty IdContrato values are '0'
+# Labels dataset
+Y = np.array(df['IdContrato'].astype(str).tolist())  # # empty IdContrato values are '0'
+
+
+# note that if X.shape and Y.shape are not the same, it will error.
+# make sure to have all the embeddings
+X_non_zero, X_zeros, Y_non_zero, Y_zeros = divideDataset(X, Y)
+
+
+n_clusters = 20  # Assuming 10 clusters
+input_dim = 384  # Your embedding dimension
+
+
+print(X_non_zero.shape)
+print(Y_non_zero.shape)
+
+
+# Set the seed for reproducibility
+np.random.seed(42)
+
+# Determine how many samples to select (half of the dataset)
+sample_size = X_non_zero.shape[0] // 10
+
+# Randomly sample indices
+indices = np.random.choice(X_non_zero.shape[0], sample_size, replace=False)
+
+# Use the sampled indices to create a new subset of the data
+X_non_zero_reduced = X_non_zero[indices]
+Y_non_zero_reduced = Y_non_zero[indices]
+
+
+print(X_non_zero_reduced.shape)
+print(Y_non_zero_reduced.shape)
+
 
 
 # Initialize the DeepEmbeddingClustering model
-n_clusters = 10  # Assuming 10 clusters
-input_dim = X  # Your embedding dimension
-
-
 c = DeepEmbeddingClustering(n_clusters=n_clusters, input_dim=input_dim)
 
 
 # Initialize the model with the embeddings
-c.initialize(X, finetune_iters=100, layerwise_pretrain_iters=50)  # Adjust iterations as necessary
+c.initialize(X_non_zero_reduced, finetune_iters=100, layerwise_pretrain_iters=50)  # Adjust iterations as necessary
 
 # Perform clustering
-#c.cluster(X, y=Y, iter_max=10)
+c.cluster(X_non_zero_reduced, y=Y_non_zero_reduced, iter_max=10)
